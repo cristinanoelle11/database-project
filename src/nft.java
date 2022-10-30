e20c202e3659fe53decef19a7000daff3e9f71a5
cristinanoelle11 <30802336+cristinanoelle11@users.noreply.github.com>
2022-10-30 13:19:08 -0400

    renamed nft

---
+++ src/nft.java
@@ -0,0 +1,78 @@
+
+public class nft {
+	protected int nftID;
+	protected String name;
+	protected String description;
+	protected String image;
+	protected int owner;
+	protected int price;
+	
+	//constructors
+	public nft() {}
+	public nft(int owner) {
+		this.owner = owner;
+	}
+	
+	public nft(String name, String description, String image, int owner) {
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.owner = owner;
+	}
+	
+	public nft(int nftID, String name, String description, String image, int owner) {
+		this.nftID = nftID;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.owner = owner;
+	}
+	public nft(int nftID, String name, String description, String image, int owner, int price) {
+		this.nftID = nftID;
+		this.name = name;
+		this.description = description;
+		this.image = image;
+		this.owner = owner;
+		this.price = price;
+	}
+	//getters and setters
+	
+	public int getNftID() {
+		return nftID;
+	}
+	public void setNftID(int nftID) {
+		this.nftID = nftID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	public int getOwner() {
+		return owner ;
+	}
+	public void setOwner(int owner) {
+		this.owner = owner;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
+
